d017f64aaa2b2b0f1bf5baf16fa0ec647c9986ae
camou <mgmt.camou@gmail.com>
2025-01-31 16:07:17 +0100

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'camou <vergeilt>'
+description 'Modern, minimalistic FiveM framework'
+
+client_scripts {
+    'client/**/*.lua'
+}
+
+server_scripts {
+    'server/**/*.lua'
+}
+
+ui_page 'ui/web/index.html'
+
+files {
+    'ui/web/index.html',
+    'ui/web/**/*',
+    'config/*.lua'
+}
+
+dependencies {
+    '/server:12168',
+    '/onesync'
+}
